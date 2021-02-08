3e564ae8d6bc19971995f1b7e70bca613f6940eb
HyubjinLee <hyubjin487@gmail.com>
2021-02-08 12:49:33 +0900

    query separate

---
+++ data_processing/query_separator.py
@@ -0,0 +1,20 @@
+import pandas as pd
+
+single = {"table": [], "predicate": [], "cardinality": []}
+join = {"table": [], "join": [], "predicate": [], "cardinality": []}
+
+df = pd.read_csv("../data/query.csv", sep='#', names=["table", "join", "predicate", "cardinality"])
+
+for i in range(len(df)):
+    table = df["table"][i].split(',')
+    if len(table) == 1:  # single query
+        for key in ["table", "predicate", "cardinality"]:
+            single[key].append(df[key][i])
+    else:  # join query
+        for key in ["table", "join", "predicate", "cardinality"]:
+            join[key].append(df[key][i])
+
+single = pd.DataFrame(single)
+join = pd.DataFrame(join)
+single.to_csv("../data/single_query.csv", sep='#', index=False)
+join.to_csv("../data/join_query.csv", sep='#', index=False)
